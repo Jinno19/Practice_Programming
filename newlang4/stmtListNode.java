6f9bee8536b24abd27c65b9d3f89219510a65e2e
yuukari <c01171576b@edu.teu.ac.jp>
2019-12-20 00:27:18 +0900

    add stmtListNode

--- newlang4/stmtListNode.java
+++ newlang4/stmtListNode.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class stmtListNode extends Node {
 	Node stmt,block;
 	Environment env;
-	
+
 	static final Set<LexicalType> firstSet=EnumSet.of(
 			LexicalType.NAME,
 			LexicalType.FOR,
@@ -15,18 +15,40 @@ public class stmtListNode extends Node {
 			LexicalType.IF,
 			LexicalType.DO
 			);
-	
+
 
 	public static boolean isFirst(LexicalUnit first) {
-		
+
 		return firstSet.contains(first.getType());
 	}
 
 	public static Node getHandler(LexicalUnit first, Environment env) {
-		
+
 		return new stmtListNode(first,env);
 	}
-	
-	
 
-}
+	private stmtListNode(LexicalUnit first,Environment env) {
+		this.env=env;
+	}
+
+
+
+
+	@Override
+	public boolean parse() throws Exception{
+
+		LexicalUnit first=env.getInput().get();
+
+		while(true){
+		if(stmtListNode.isFirst(first)) {
+			stmt=stmtListNode.getHandler(first,env);
+					return stmt.parse();
+		}else {
+			return false;
+		}
+		}
+		}
+		public String toString() {
+			return stmt.toString();
+		}
+	}
