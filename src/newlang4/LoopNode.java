731ce92f9ec9d6df11d088da0dd29dcce7ce5fb6
yuukari <c01171576b@edu.teu.ac.jp>
2020-02-07 19:57:18 +0900

    add LoopNode

--- src/newlang4/LoopNode.java
+++ src/newlang4/LoopNode.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public class LoopNode extends Node{
 	Node handler;
+	Node catchStmtList; //stmtListの値保持
 	Environment env;
 
 	static final Set<LexicalType> firstSet = EnumSet.of(LexicalType.DO, LexicalType.WHILE);
@@ -39,12 +40,60 @@ public class LoopNode extends Node{
 			throw new Exception("Not found UNTIL.");
 		}
 
-		first=env.getInput().get();
-		env.getInput().unget(first);
-		if(!CondNode.isFirst(first)) {
+		LexicalUnit second=env.getInput().get();
+		env.getInput().unget(second);
+		if(!CondNode.isFirst(second)) {
 			throw new Exception("Undefined Cond.");
 		}
+		handler=CondNode.getHandler(second, env);
+		handler.parse();
+
+		first = env.getInput().get();
+		while (true) {
+			if (first.getType() == LexicalType.NL) {
+				first = env.getInput().get();
+				continue;
+			} else {
+				env.getInput().unget(first);
+				break;
+			}
+		}
+
+		first = env.getInput().get();
+		env.getInput().unget(first);
+		if (!stmtListNode.isFirst(first)) {
+			throw new Exception("Not stmtList.");
+		}
+		catchStmtList = stmtListNode.getHandler(first, env);
+		catchStmtList.parse();
+
+		first=env.getInput().get();
+		if(first.getType()!=LexicalType.LOOP) {
+			throw new Exception("Not found LOOP.");
+		}
+
+		first = env.getInput().get();
+		while (true) {
+			if (first.getType() == LexicalType.NL) {
+				first = env.getInput().get();
+				continue;
+			} else {
+				env.getInput().unget(first);
+				break;
+			}
+		}
+
+		return true;
 	}
+
+	@Override
+	public String toString() {
+		// TODO 自動生成されたメソッド・スタブ
+
+		return "DO UNTIL"+handler.toString()+catchStmtList.toString()+"LOOP";
+	}
+
+
 
 
 }
